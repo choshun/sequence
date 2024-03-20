4088ce536d7db69fc6ab040e69f47946149e0346
Choshun Snyder <choshun.snyder@gmail.com>
2024-03-20 11:48:39 -0400

    See if this helps netlify return.

--- src/routes/api/supabase/+server.ts
+++ src/routes/api/supabase/+server.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 import { env } from '$env/dynamic/private';
 import { BehaviorSubject, Observable, ReplaySubject, first, last, takeLast } from 'rxjs'
+import type { Context } from "@netlify/edge-functions";
 
 let sequenceSubject = new BehaviorSubject<any>(1)
 const _sequence$: Observable<any> = sequenceSubject.asObservable();
@@ -29,14 +30,21 @@ const channel = _supabase
 
 // Only subscribe once - cleans up too
 // https://stackoverflow.com/questions/28007777/create-one-time-subscription
+
+
+// UPDATE: prolly not | add this here? https://stackoverflow.com/questions/74879852/how-can-i-implement-server-sent-events-sse-in-sveltekit
+// from link: https://github.com/sveltejs/kit/issues/5344
+
+// gawdammit
+// https://edge-functions-examples.netlify.app/example/server-sent-events
 export function GET(id: number) {
+	const encoder = new TextEncoder();
 	const stream = new ReadableStream({
         async start(controller) {
             // You can enqueue multiple data asynchronously here.
             _sequence$.pipe().subscribe((session) => {
 				console.log(JSON.stringify(session.sequence))
-				
-				controller.enqueue(`${JSON.stringify(session)}`)
+				controller.enqueue(encoder.encode(`${JSON.stringify(session)}`))
 			})
             // controller.close() 
         },
