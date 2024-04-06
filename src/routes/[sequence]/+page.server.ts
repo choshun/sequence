04a2f5900c1e4493ea7e8981f77430ac0c35f2d8
Choshun Snyder <choshun.snyder@gmail.com>
2024-04-06 10:37:09 -0400

    cleanup

--- src/routes/[sequence]/+page.server.ts
+++ src/routes/[sequence]/+page.server.ts
@@ -1,73 +1,14 @@
-// Only update one entry
-/**
- * 1. make route with /frist 
- * 2. fetch from here - maybe user_id/name
- * 3. maybe make unique constraint user_id/name
- * 3. update JSON on front end, hit save, reload - should be there
- * 4. have it in another browser make sure both windows update
- */
-
-/**
- * other stuff, login, get user id
- * https://supabase.com/docs/reference/javascript/auth-api - just use that and github
- */
-
 import { error } from '@sveltejs/kit'
 import { _fetchSequenceById, _updateSequenceById, _sequence$, type PageData, type Sequence, _fetchSequenceStreamById } from "../api/sequence/+server";
 let count: number = 0
-let sequence: any
-let updatedSequence: any
 
-console.log("hi");
-
-// _sequence$.subscribe((newSequence) => {
-// 	console.log("update", newSequence.sequence)
-// 	count++;
-// 	console.log("test", count)
-// 	// sequence = newSequence
-// 	// sequence = newSequence;
-// 	updatedSequence = newSequence
-// })
 
 // https://stackoverflow.com/questions/74330190/how-to-respond-with-a-stream-in-a-sveltekit-server-load-function
 // https://kit.svelte.dev/docs/routing#server
 export async function load({params}): Promise<PageData | null> {
-	// load is called every update - need to put subscribe outside of load
-	// if (count < 3) {
 	const sequence = await _fetchSequenceById(+params.sequence) as any
-	// const stream = await _fetchSequenceStreamById(97)
-	// let test: WritableStream<any> = new WritableStream()
-	// console.log(stream)
-	
-
-	
-	// } else {
-		// sequence = {
-		// 	sequence: {
-		// 		id: 97,
-		// 		history: "",
-		// 		sequence: {
-		// 			"it": "updates?"
-		// 		},
-		// 		name: "shwa"
-		// 	}
-		// }
-
-		// sequence = updatedSequence
-
-	//}
-	// if (!sequence) {
-	// const stream = await sequence;
-	// fuck, might need to do stream here
-	// console.log(stream)
-	console.log(sequence)
-	console.log("fired again?")
-	//}
-	console.log("test", count)
-	console.log(sequence)
 	count++
 	if (sequence) {
-		console.log(sequence.sequence);
 		return {
 			sequence,
 			count,
@@ -75,19 +16,13 @@ export async function load({params}): Promise<PageData | null> {
 		}
 	}
 
-	
-	console.log(sequence)
-
-
 	error(404, 'Not found');
 }
 
 export const actions = {
 	update: async ({ params, request }) => {
-		console.log(params.sequence)
 		const formData: any = await request.formData();
 		const sequenceData = JSON.parse(formData.get('sequence'))
-		console.log(JSON.parse(formData.get('sequence')))
 		const sequence = await _updateSequenceById(+params.sequence, sequenceData)
 	}
 }
--- src/routes/api/supabase/+server.ts
+++ src/routes/api/supabase/+server.ts
@@ -1,14 +1,12 @@
 import { createClient } from '@supabase/supabase-js'
 import { env } from '$env/dynamic/private';
 import { BehaviorSubject, Observable, ReplaySubject, first, last, takeLast } from 'rxjs'
-// import type { Context } from "@netlify/edge-functions";
 
 let sequenceSubject = new BehaviorSubject<any>(1)
 const _sequence$: Observable<any> = sequenceSubject.asObservable();
 const projectUrl = env?.SUPABASE_PROJECT_URL ? env.SUPABASE_PROJECT_URL : process.env.SUPABASE_PROJECT_URL as string;
 const projectKey = env?.SUPABASE_API_KEY ? env.SUPABASE_API_KEY : process.env.SUPABASE_API_KEY as string;
 
-// Might have to do one for dev, the other for prod?
 export const _supabase = createClient(projectUrl, projectKey, {
 	global: {
 		fetch: (...args) => fetch(...args),
@@ -29,18 +27,6 @@ const channel = _supabase
 		})
 		.subscribe()
 
-// Only subscribe once - cleans up too
-// https://stackoverflow.com/questions/28007777/create-one-time-subscription
-
-
-// UPDATE: prolly not | add this here? https://stackoverflow.com/questions/74879852/how-can-i-implement-server-sent-events-sse-in-sveltekit
-// from link: https://github.com/sveltejs/kit/issues/5344
-
-// gawdammit
-// https://edge-functions-examples.netlify.app/example/server-sent-events
-
-// just updating encoding does not work:
-// https://docs.netlify.com/edge-functions/get-started/
 export function GET(id: number) {
 	const encoder = new TextEncoder();
 	const stream = new ReadableStream({
